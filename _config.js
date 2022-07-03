5e061b4aad29e35bae84a0f630db8123d729fc56
Zoo Sky <wortblau@gmail.com>
2022-07-03 22:41:42 +0200

    add configs

--- _config.js
+++ _config.js
@@ -3,6 +3,14 @@ import lume from "lume/mod.ts";
 const site = lume({
   src: "./src",
   dest: "./public",
+  server: {
+    open: true,
+    port: 4000,
+  },
 });
+
+site.copy("assets");
+site.copy("favicon.ico");
+site.ignore("README.md", "CHANGELOG.md");
 
 export default site;
